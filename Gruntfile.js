8802cc27f0a68d67e7607807f31978b7c42158e7
WM Chang <wandererm@gmail.com>
2013-09-12 22:54:54 +0800

    add grunt files

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		watch: {
+			livereload: {
+				files: ['*.shtml'],
+				tasks: ['copy', 'ssi'],
+				options: {
+					livereload: true
+				}
+			}
+		},
+		connect: {
+			server: {
+				options: {
+				    hostname: '*'
+				}
+			}
+		},
+		copy: {
+			main: {
+				files: [{
+					src: 'index.shtml',
+					dest: 'index.html'
+				}]
+			}
+		},
+		ssi: {
+			options: {
+				input: './',
+				output: './',
+				matcher: 'index.html'
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.registerTask('ssi', 'Flatten SSI includes in your HTML files.', function() {
+        
+	var ssi = require( 'ssi' )
+		, opts = this.options()
+		, files = new ssi( opts.input, opts.output, opts.matcher )
+		;
+
+	files.compile();
+
+	});
+	grunt.registerTask('default', ['copy', 'ssi', 'connect', 'watch']);
+};
